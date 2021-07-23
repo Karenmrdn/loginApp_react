5a8df6fecf5a5ec0f8ab4b665c482aad8187c89b
karenmrdn <karenmrdn47@gmail.com>
2021-07-23 18:49:37 +0300

    First part of an app: context used

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+
+function App() {
+  return (
+    <>
+      <MainHeader onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </>
+  );
+}
+
+export default App;
